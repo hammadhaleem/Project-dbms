ac6b1c3c5e1356ac8d55aec3a470019eb0be7579
hamamad <hammad@hammad.(none)>
2012-07-23 23:54:53 +0530

    templating

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 import web
 
-def make_text(string):
-    return string
-
-urls = ('/', 'index')
 render = web.template.render('templates/')
 
-app = web.application(urls, globals())
+urls = (
+    '/(.*)', 'index'
+)
 
-my_form = web.form.Form(
-                web.form.Textbox('', class_='textfield', id='textfield'),
-                )
+class index:
+   def GET(self, name):
+  	 return render.index(name)
+	
 
-class tutorial:
-    def GET(self):
-        form = my_form()
-        return render.tutorial(form, "Your text goes here.")
-        
-    def POST(self):
-        form = my_form()
-        form.validates()
-        s = form.value['textfield']
-        return make_text(s)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = web.application(urls, globals())
     app.run()
